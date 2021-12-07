9cef210a5c427cd46745415cb7ac4ef40504d133
Xzaixz <75606948+Xzaixz@users.noreply.github.com>
2021-12-07 13:37:17 +0300

    Add files via upload

---
+++ gonmi/XZGonki/XZGonki/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  XZGonki
+//
+//  Created by Student on 30.11.2021.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+}
+ 
+
